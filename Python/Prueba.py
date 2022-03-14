1c728b3afa6b27599e4b90f9803b9ad841d67590
Alvaro <montero.barraza.alvaro.david@gmail.com>
2022-03-13 22:38:58 -0600

    add- try and except sentences

--- Python/Prueba.py
+++ Python/Prueba.py
@@ -1,10 +1,10 @@
 range=1,2,3,4,5,6,7
 mal=int(input("Digite un 1 o un 0"))
+
 if (mal==0):
     valor=True
 else:
     valor=False
-
 if (valor):
         print(valor)
         variable=int(input("Digite un numero"))
@@ -13,3 +13,17 @@ else:
         variable=-3
 variable=variable*2
 print(variable)
+
+try:
+    prueba=(input("Digite un numero"))
+    prueba=int(prueba)
+    prueba=prueba+prueba/3
+except:
+    print("error con el tipo de dato")
+    bandera=True
+if bandera:
+    prueba=int(input("Digite un numero"))
+    print(prueba)
+    
+else:
+    print(prueba)
