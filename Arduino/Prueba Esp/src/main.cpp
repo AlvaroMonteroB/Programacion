372bacb3f1d09a24a01106ce6f8df654dc64111b
Alvaro <montero.barraza.alvaro.david@gmail.com>
2022-04-15 00:32:35 -0500

    mod_wifi

--- Arduino/Prueba Esp/src/main.cpp
+++ Arduino/Prueba Esp/src/main.cpp
@@ -1,7 +1,23 @@
 #include <Arduino.h>
-
+#include<ESP8266WiFi.h>
+const char *ssid="ssid";
+const char *password="password";
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(10);
+
+  WiFiMode(WIFI_AP);
+  while (!WiFi.softAP(ssid,password))
+  {
+    Serial.print(".");
+    delay(100);
+
+  }
+  Serial.print("Iniciando AP");
+  Serial.print(ssid);
+  Serial.print("Ip address:\t");
+  Serial.println(WiFi.softAPIP());
+  
 }
 
 void loop() {
